85b4d10e4adb6b3e10f2b63d669b87625a31792f
Brayan daniel fuquene sandoval <brayan.fuquene01@uptc.edu.co>
2023-04-21 11:04:07 +0200

    kickoff for micro service  of tickets

---
+++ tickets-ms/src/main/java/co/uptc/edu/ticketsms/MsTicketApplication.java
@@ -0,0 +1,13 @@
+package co.uptc.edu.ticketsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTicketApplication.class, args);
+	}
+
+}
