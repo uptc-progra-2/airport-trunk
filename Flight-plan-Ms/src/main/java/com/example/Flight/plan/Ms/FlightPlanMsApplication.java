75582c7b527393cf3bb5573020fc624ccc9a8d63
yeison piracon <yeison.piracon01@uptc.edu.co>
2023-04-21 11:25:17 -0500

    creacion para el repositorio plan de vuelo

---
+++ Flight-plan-Ms/src/main/java/com/example/Flight/plan/Ms/FlightPlanMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flight.plan.Ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlanMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlanMsApplication.class, args);
+	}
+
+}
