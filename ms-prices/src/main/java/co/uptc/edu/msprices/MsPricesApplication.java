a1edb39594124a98177ecf6ab94e581ea64ec4f4
David Santiago Cubillos <leonreyds@gmail.com>
2023-04-21 11:49:34 -0500

    Rama de price

---
+++ ms-prices/src/main/java/co/uptc/edu/msprices/MsPricesApplication.java
@@ -0,0 +1,13 @@
+package co.uptc.edu.msprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPricesApplication.class, args);
+	}
+
+}
