66a1d700218e71dd516ff5fed218f579280dd619
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-04-21 11:05:06 -0500

    agragndo dependencias

---
+++ users-ms/src/main/java/co/uptc/edu/ms/users/usersms/UsersMsApplication.java
@@ -0,0 +1,13 @@
+package co.uptc.edu.ms.users.usersms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMsApplication.class, args);
+	}
+
+}
