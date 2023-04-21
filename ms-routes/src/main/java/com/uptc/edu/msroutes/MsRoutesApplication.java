916b68a07a44187596cbe81f1d862daa7713af70
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-04-21 11:23:56 -0500

    Initializer

---
+++ ms-routes/src/main/java/com/uptc/edu/msroutes/MsRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.edu.msroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRoutesApplication.class, args);
+	}
+
+}
