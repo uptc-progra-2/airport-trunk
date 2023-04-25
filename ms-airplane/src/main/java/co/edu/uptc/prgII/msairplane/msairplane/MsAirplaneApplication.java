2863f0376ef7d21d30a3b8d958fa0bcdba45ad27
andres barrera <andresbarrera427@gmail.com>
2023-04-24 21:36:37 -0500

    primer commit

---
+++ ms-airplane/src/main/java/co/edu/uptc/prgII/msairplane/msairplane/MsAirplaneApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.prgII.msairplane.msairplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAirplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAirplaneApplication.class, args);
+	}
+
+}
